b03043ed34926256d5bb9e28aa64c1c2e2bb753d
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-06 23:53:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length >= 1
+    arrayed = []
+    counter = 1
+    array.each do |name|
+      arrayed << "#{counter}. #{name}"
+      counter += 1
+    end
+    puts "The line is currently: #{nuarray.join(' ')}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+
+
+
+
+
+
+
+
+
+# initialize katz_deli= []
+# define line(array)
+# if array_length >= 1
+# then create a new array
+# then start a counter at 1
+#
+#
+#
+#
