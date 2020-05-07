96c1aa4a6f9f5b365d21f98f877906fcc4d88a8f
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 01:08:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,11 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  katz_deli.shift
-  puts "Currently serving #{katz_deli[0]}"
+  if katz_deli.length >= 1
+    katz_deli.shift
+    puts "Currently serving #{katz_deli[0]}"
+  else
+    puts "There is nobody waiting to be served!"
 end
 
 
