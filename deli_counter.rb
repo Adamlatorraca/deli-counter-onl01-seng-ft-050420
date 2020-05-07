9bad5b133fb6934b2b18fc1502a0bf52b69d24a3
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 01:02:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,8 @@ def take_a_number(katz_deli, name)
       puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-
+def now_serving(katz_deli)
+  
 
 #
 # def take a number with arguments current people in line and string name joinng end of array
