4ddae0869f3bb6fc95c398e80d418df8646ab7db
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 00:31:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,11 @@ end
 
 def take_a_number(katz_deli, name)
   if katz_deli.length >= 1
-    return "Welcome, #{name}. You are number #{katz_deli.join(' ')} in line"
+      arrayed = []
+      counter = 1
+      katz_deli.each do |name|
+          arrayed <<
+    puts "Welcome, #{name}. You are number #{katz_deli.join(' ')} in line"
   else
     puts "The line is currently empty."
   end
