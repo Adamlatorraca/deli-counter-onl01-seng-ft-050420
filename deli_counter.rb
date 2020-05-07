085d2770f888f1af39610d02510ac8eabdc7c055
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 00:56:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    katz_deli << string
-    puts "Welcome, #{string}. You are number #{katz_deli.length(' ')} in line"
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line"
 
 end
 
